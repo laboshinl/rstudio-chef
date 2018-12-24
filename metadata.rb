53e0300b24eb63528af4bfc97377db599d4a2d36
Leonid Laboshin <laboshinl@gmail.com>
2018-12-24 15:14:32 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'joe@joestump.net'
 license          'BSD'
 description      'Installs/Configures rstudio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '0.4.2'
 
 depends "apt"
 depends "nginx"
